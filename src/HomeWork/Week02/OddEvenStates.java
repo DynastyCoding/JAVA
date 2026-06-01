87199c0f39f0099dd3b9153998c2477c0ecf0d2b
DynastyDuke <dynasty03072@gmail.com>
2026-06-01 22:51:02 +0800

    feat: complete OddEvenStates

--- src/HomeWork/Week02/OddEvenStates.java
+++ src/HomeWork/Week02/OddEvenStates.java
@@ -7,26 +7,26 @@ public class OddEvenStates {
         Scanner in = new Scanner(System.in);
         System.out.println("please enter a positive integer:");
 
-        int inte = in.nextInt();
+        int count = in.nextInt();
         int oddSum = 0;
         int evenSum = 0;
         int evenCount = 0;
 
-        for (int i = 1; i <= inte; i++) {
-            System.out.println("type your numbers:");
+        for (int i = 1; i <= count; i++) {
+            System.out.printf("enter number %d: ", i);
             int num = in.nextInt();
 
             if (num % 2 == 1) {
-               oddSum = oddSum + num;
-            }else{
+                oddSum = oddSum + num;
+            } else {
                 evenSum = evenSum + num;
-                evenCount = evenCount +1;
+                evenCount = evenCount + 1;
             }
         }
-        System.out.printf("all odd summation = %d%n",oddSum);
-        if(evenCount>0) {
-            System.out.printf("all even average = %.2f", (double) evenSum / evenCount);
-        }else{
+        System.out.printf("all odd summation = %d%n", oddSum);
+        if (evenCount > 0) {
+            System.out.printf("all even average = %.2f%n", (double) evenSum / evenCount);
+        } else {
             System.out.println("even average output=0.0");
         }
         in.close();
